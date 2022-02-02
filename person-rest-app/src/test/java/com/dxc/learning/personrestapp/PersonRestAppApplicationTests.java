60eaccd5b8c9e1bd881b6e79c82f7ba96880255a
Deepikajagadam <96856837+Deepikajagadam@users.noreply.github.com>
2022-02-02 18:09:25 +0530

    Add files via upload

---
+++ person-rest-app/src/test/java/com/dxc/learning/personrestapp/PersonRestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dxc.learning.personrestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonRestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
